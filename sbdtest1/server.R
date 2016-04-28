b23fb8f653606e43c03fc4457f01f441995e49a4
jeffnorville <jeff@jnorville.com>
2016-04-28 10:32:25 +0200

    first load

---
+++ sbdtest1/server.R
@@ -0,0 +1,96 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+#connect to db
+#library(dplyr)
+library(RPostgreSQL)
+library(lazyeval)
+library(ggplot2)
+
+# db <- src_postgres('postgres',
+#                    host = 'localhost',
+#                    port = 5432,
+#                    user = 'postgres', 
+#                    password = 'irstea')
+# tbl_scores <- tbl(db, "tblScores")
+# print(translate_sql( tbl = tbl_scores, window = TRUE))
+
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+
+  #using fulldb too big - stick with subset until figure out dplyr issue
+  #get the filters to df
+  filtInput <- reactive({
+    scoreType <- input$ctlScrtype
+    locationID <- input$ctlLocid
+    # minyear <- input$year[1]
+    # maxyear <- input$year[2]
+    
+  
+  # Apply filters
+  sm <- db2005 %>%
+    filter(
+      ScoreType == input$ctlScrtype,
+      Location == input$ctlLocid
+      # Year <= maxyear,
+    ) %>%
+    arrange(locationID)
+#    sm <- subset(fulldb, locationID %in% c(ctlLocid))
+    #sm <- subset(fulldb, locationID %in% c('S2242510'))
+#    sm2 <- subset(sm, dateValue > "2005-01-01" & dateValue < "2005-12-31")
+    #sm2 = subset(sm, dateValue > "2005-07-01" & dateValue < "2005-07-31") #no values???
+    
+    
+  output$distPlot <- renderPlot({
+    
+    ggplot(sm2,aes(x = LT / 7, y = dateValue)) + 
+      geom_point(aes(color = scoreValue), size=5) +
+      scale_x_continuous("Lead Time (weeks)") + scale_y_date("Months of 2005 (January omitted)") +
+      scale_color_gradient(low="yellow", high="darkgreen")
+    
+  }) #end reactive bit
+
+    
+    # x    <- faithful[, 2]  # Old Faithful Geyser data
+    # bins <- seq(min(x), max(x), length.out = input$bins * 7 + 1)
+    # # draw the histogram with the specified number of bins
+    # hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  })
+  
+    #db
+    # scores <- tbl(db, "tblScores")
+    # print(translate_sql(rank(), tbl = "tblScores", window = TRUE))
+    # #controls  unique(as.character(mpg$manufacturer
+    # scrflav <- collect(order(unique(as.character(scores$scoreType))))
+    # loc <- unique(as.character(scores$locationID))
+    
+    
+    # filter stuff to draw
+    # remote <- select(filter(scores, locationID == 'M0243010' && 
+    #                            scoreType == 'Seasonal_LS_month' && 
+    #                            LT == 4), dateValue, scoreValue:1)
+    #remote2 <- collapse(remote) #unimportant for now
+    #cached <- compute(remote2)
+    # local  <- collect(remote) #finally hits db with SELECT WHERE
+
+
+  })
+
+  # generate sampling based on input$leadtimes from ui.R
+  # x    <- scores[, 2] 
+  # bins <- seq(min(x), max(x), length.out = (input$leadtimes + 1)/7)
+  
+  
+
+
+#on unload type of disonnect???
+
+
---
+++ sbdtest1/ui.R
@@ -0,0 +1,111 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+#library(dplyr)
+library(RPostgreSQL)
+library(lazyeval)
+library(ggplot2)
+
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Scoreboard"),
+
+  fluidRow(
+    column(4,
+        wellPanel( 
+          h4("Filter"),
+           selectInput("ctlScrtype",
+                       "Score Type:",
+                       c("Seasonal_EDMD_month",
+                          "Seasonal_EDMD_month_ByWeek",
+                          "Seasonal_LS_month",
+                          "Seasonal_LS_month_ByWeek"
+                        )
+                       ),
+          selectInput("ctlLocid",
+                      "Location:",
+                      c( "A1080330",
+                          "B2220010",
+                          "H2342020",
+                          "H4252010",
+                          "H7401010",
+                          "H8212010",
+                          "I5221010",
+                          "J7483010",
+                          "K1321810",
+                          "K6402520",
+                          "L0563010",
+                          "L4411710",
+                          "M0243010",
+                          "M7112410",
+                          "S2242510",
+                          "U4644010"
+                      )
+          )
+          
+          )),
+
+  # Sidebar with a slider input for number of bins / leadtimes?
+  sidebarLayout(
+    sidebarPanel(
+
+      # selectInput("select", label = h3("Select box"), 
+      #             choices = ls_locations, selected = 1)
+      #   ),
+    
+
+       sliderInput("bins",
+                   "Show leadtimes (weeks):",
+                   min = 1,
+                   max = 90 / 7,
+                   value = 30 / 7)
+       
+    ),
+
+#      ls_locations <- c("A1080330","B2220010", "H2342020"),    
+#    ls_locations <-    c("A1080330" = A1080330,
+#                         "B2220010" = B2220010,
+#                         "H2342020" = H2342020,
+#                         "H4252010" = H4252010,
+#                         "H7401010" = H7401010,
+#                         "H8212010" = H8212010,
+#                         "I5221010" = I5221010,
+#                         "J7483010" = J7483010,
+#                         "K1321810" = K1321810,
+#                         "K6402520" = K6402520,
+#                         "L0563010" = L0563010,
+#                         "L4411710" = L4411710,
+#                         "M0243010" = M0243010,
+#                         "M7112410" = M7112410,
+#                         "S2242510" = S2242510,
+#                         "U4644010" = U4644010
+# ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+       plotOutput("distPlot") #,
+
+              #subsubtoto has one locationID, one lead time, one scoreType
+       # filtered (timeseries) of scoreValues against dateValues
+       # ggplot(data=local,aes(x=dateValue,y=scoreValue)) + geom_point(aes(color=LT),size=1) +
+       #   scale_x_date("Month") + scale_y_continuous("Score")
+       
+
+       # choose columns to display
+       # diamonds2 = diamonds[sample(nrow(diamonds), 1000), ],
+       # output$mytable1 <- DT::renderDataTable({
+       #   DT::datatable(diamonds2[, input$show_vars, drop = FALSE])
+       # })
+    ))
+  )
+))
