3e63f1df610312079f2c632951913260ac37dc83
jeffnorville <jeff@jnorville.com>
2016-06-01 07:27:55 +0200

    automated commit

--- scratch.R
+++ scratch.R
@@ -1,10 +1,9 @@
+load("~/SMHI Data/fcst.RData")
 
-as.numeric(as.Date(fcst$Start_date))
-
-ts.fcst <- ts(fcst, start = as.numeric(as.Date(fcst$Start_date)), end = as.numeric(as.Date(fcst$end_date)))
-
-by(fcst$rmse[,1:3], )
-
+# as.numeric(as.Date(fcst$Start_date))
+# 
+# ts.fcst <- ts(fcst, start = as.numeric(as.Date(fcst$Start_date)), end = as.numeric(as.Date(fcst$end_date)))
+# by(fcst$rmse[,1:3], )
 
 # 1 reduce data to one test
 
@@ -14,13 +13,20 @@ fifi <- fcst$rmse
 basins <- fcst$subids
 
 # 3 rename columns
-colnames(fifi) <- c(1:6)
-
+colnames(fifi) <- as.factor(c(1:6))
 #cbind() the basin names to each years' months
 
 #4 add basins to rows, repeating each 12
 #4 add months to rows, repeating for columns
-rownames(fifi) <- c(1:12)
+basins.expanded <- rep(basins, times = 1, each = 12)
+
+# mistake ?? shoud have 6 lead times, 
+
+fifi2 <- as.data.frame(fifi)
+fifi2$locationID <- basins.expanded
+
+fifi3 <- melt(fifi2, id.vars=c("locationID"))
+# add dates
 
 
 # gotta convert to df!
