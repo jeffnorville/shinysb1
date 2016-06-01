60ceb0c3a27a559dad3d6c3510845310b61d17d3
jeffnorville <jeff@jnorville.com>
2016-06-01 13:58:37 +0200

    importing new data

--- scratch.R
+++ scratch.R
@@ -6,7 +6,6 @@ load("~/SMHI Data/fcst.RData")
 # by(fcst$rmse[,1:3], )
 
 # 1 reduce data to one test
-
 fifi <- fcst$rmse
 
 # 2 list of basins
@@ -20,17 +19,27 @@ colnames(fifi) <- as.factor(c(1:6))
 #4 add months to rows, repeating for columns
 basins.expanded <- rep(basins, times = 1, each = 12)
 
-# mistake ?? shoud have 6 lead times, 
-
 fifi2 <- as.data.frame(fifi)
 fifi2$locationID <- basins.expanded
+fifi2$months <- rep(1:12, times=825)
 
-fifi3 <- melt(fifi2, id.vars=c("locationID"))
-# add dates
+library(reshape2)
+library(dplyr)
+fifi3 <- melt(fifi2, id.vars=c("locationID", "months"))
 
+# colnames(fifi3)[3] <- "newName"
+colnames(fifi4)[3] <- "leadtimeValues"
+colnames(fifi4)[4] <- "scoreValues"
+fifi4 <- arrange(fifi4, months, leadtimeValues, locationID)
 
-# gotta convert to df!
+# play with df for plots
+
+fifi4 <- filter(fifi3, locationID == 8200208)
+# fifi4 <- fifi4[order("locationID", "months")]
 require(ggplot2)
-ggplot(fifi, aes(fifi[1])) +                    # basic graphical object
-  geom_line(aes(y=y1), colour="red") +  # first layer
-  geom_line(aes(y=y2), colour="green")  # second layer
+ggplot(fifi4, aes(leadtimeValues, scoreValues))
+
+
+ggplot(fifi4, aes(leadtimeValues, scoreValues)) +                    # basic graphical object
+  geom_line(aes(months=1), colour="blue") +  # first layer
+  geom_line(aes(months=2), colour="green")  # second layer
