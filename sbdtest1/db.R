02ee13a4f661bd7b9518a8b4d675847723dde0e2
jeffnorville <jeff@jnorville.com>
2016-06-15 15:39:29 +0200

    stuck w ggplot format trouble (dodge, colour); reactive facets

--- sbdtest1/db.R
+++ sbdtest1/db.R
@@ -86,12 +86,16 @@ if(leadtimeUnit == "daily") {
 
 
 reduced <- filter(getit, scoreType == "CRPS")
-reduced$month <- format(reduced$dateValue, "%m")
 
-reduced <- filter(getit, Month(dateValue) == 2)
+
+
+# reduced <- filter(getit, Month(dateValue) == 2)
+
 
 
 local <- collect(reduced)
+local$month <- format(local$dateValue, "%m")
+
 
 #  as.POSIXlt(date1)$mon
 # season.winter <- c(12, 1, 2) # december, january, february
@@ -105,16 +109,22 @@ loc.sum <- summarySE(local, measurevar="scoreValue", groupvars=c("locationID", "
 loc.sum$locationID <- as.factor(loc.sum$locationID)
 group <- c(1:length(loc.sum$locationID))
 
-pd <- position_dodge(0.1)
+
 
 plot(loc.sum$leadtimeValue, loc.sum$scoreValue, col=loc.sum$locationID, 
      xlab = "Lead Times", ylab = "Score")
 
 #averages
-ggplot(loc.sum, aes(x = leadtimeValue, y = scoreValue ) ) +
-  geom_point(aes(color = locationID)) +
-  geom_hline(aes(yintercept=0), colour=group, linetype="dashed") + # colour="#990000"
+pd <- position_dodge(0.2)
+ggplot(loc.sum, aes(x = leadtimeValue, y = scoreValue ), group = locationID) +
+  geom_errorbar(aes(ymin=scoreValue-ci, ymax=scoreValue+ci), position = pd) + # , color="grey"
+  geom_line(position = pd) +
+  geom_point(aes(color = locationID), position = pd) +
+  geom_hline(aes(yintercept=0), color="red", linetype="dashed") + 
   xlab("Lead Times") + ylab("Score")
+
+
+# geom_errorbar(aes(ymin=scoreValue-se, ymax=scoreValue+se), position = pd, color="grey") + # 
 
 
 #with CIs
